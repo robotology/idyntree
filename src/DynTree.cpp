b36d0cf34fccf06ef10cd0ad3555646b7d82a5fd
Silvio Traversaro <silvio.traversaro@iit.it>
2014-01-27 23:47:16 +0100

    Fixed test

--- src/DynTree.cpp
+++ src/DynTree.cpp
@@ -954,7 +954,9 @@ bool DynTree::dynamicRNEA()
         //Note: this (that no residual appears happens only for the proper selection of the provided dynContactList
         for(int i=0; i < NrOfFTSensors; i++ ) {
             #ifndef NDEBUG
-            KDL::Wrench residual = measured_wrenches[i] - ft_list.ft_sensors_vector[i]->getH_child_sensor().Inverse(f[ft_list.ft_sensors_vector[i]->getChild()]);
+            double sign = ft_list.ft_sensors_vector[i]->isWrenchAppliedFromParentToChild() ? 1.0 : -1.0;
+            
+            KDL::Wrench residual = measured_wrenches[i] - sign*ft_list.ft_sensors_vector[i]->getH_child_sensor().Inverse(f[ft_list.ft_sensors_vector[i]->getChild()]);
             assert( residual.force.Norm() < 1e-5 );
             assert( residual.torque.Norm() < 1e-5 );
             #endif //NDEBUG
