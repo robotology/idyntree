004f1ef6ec4b0bb7510bd6ea59a6ccab5cefe557
Silvio Traversaro <silvio.traversaro@iit.it>
2014-03-26 08:51:52 +0100

    [urdf_import]Fixed bug in importing COM

--- src/converters/urdf_import.cpp
+++ src/converters/urdf_import.cpp
@@ -121,7 +121,7 @@ RigidBodyInertia toKdl(boost::shared_ptr<urdf::Inertial> i)
 {
   Frame origin = toKdl(i->origin);
   // kdl specifies the inertia in the reference frame of the link, the urdf specifies the inertia in the inertia reference frame
-  return origin.M * RigidBodyInertia(i->mass, origin.p, RotationalInertia(i->ixx, i->iyy, i->izz, i->ixy, i->ixz, i->iyz));
+  return  RigidBodyInertia(i->mass, origin.p, origin.M *RotationalInertia(i->ixx, i->iyy, i->izz, i->ixy, i->ixz, i->iyz));
 }
 
 
