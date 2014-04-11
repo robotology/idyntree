b94998068f6fbee7fdf87abba6216d1ab3b9f0e4
Silvio Traversaro <silvio.traversaro@iit.it>
2014-04-11 14:32:12 +0200

    Fixed FT test

--- tests/iDynTreeWholeBodyFTsensorEstimateTest.cpp
+++ tests/iDynTreeWholeBodyFTsensorEstimateTest.cpp
@@ -1,20 +1,20 @@
-/** 
+/**
 * Copyright: 2010-2013 RobotCub Consortium
 * Author: Silvio Traversaro, Serena Ivaldi
 * CopyPolicy: Released under the terms of the GNU GPL v2.0.
-**/ 
+**/
 
 //
 // This test/example is based on the iDyn tutorial with the same name,
-// to show the similarities in the API between iDyn and iDynTree (and 
+// to show the similarities in the API between iDyn and iDynTree (and
 // for testing
 //
 // An example on using both iCubTree and iCubWholeBody to estimate the measurements of the FT sensors
-// for all the arms and legs, exploiting the modeled dynamic and the inertial sensor 
+// for all the arms and legs, exploiting the modeled dynamic and the inertial sensor
 // measurements.
 //
 // Author: Silvio Traversaro - <silvio.traversaro@iit.it>
-// Author: Serena Ivaldi 
+// Author: Serena Ivaldi
 
 #include <iostream>
 #include <yarp/sig/Vector.h>
@@ -51,20 +51,20 @@ int main()
     //// iDyn
     ////////////////////////////////////////////////////////////////////
     double tol = 1e-6;
-    
-    
+
+
     // declare an icub = head + left arm + right arm + torso + left leg + right leg
-    // the kinematic and dynamic parameters of each link are automatically set using the 
+    // the kinematic and dynamic parameters of each link are automatically set using the
     // CAD model data.
     // icub default parameters are:
     // mode    = DYNAMIC : the mode for computing wrenches, considering q,dq,d2q,mass,inertia of each link;
     //                     the other main mode is STATIC, which only considers q and mass.
-    // verbose = VERBOSE : the verbosity level; I suggest to use VERBOSE during debug, even if you see a lot 
+    // verbose = VERBOSE : the verbosity level; I suggest to use VERBOSE during debug, even if you see a lot
     //                     of messages; then to turn it off with NO_VERBOSE only if everything is really fine;
     //                     if you are using iCubWholeBody you probably don't need it a lot because the inner
     //                     classes have been tested first with iCub, but if you use iDyn in general and use
     //                     it on your own code, it's better to turn verbosity on, it may help with the library
-    //                     use. 
+    //                     use.
     version_tag ver;
     ver.head_version = 1;
     ver.legs_version = 2;
@@ -73,7 +73,7 @@ int main()
     // just priting some information to see how the class works
     // iCubWholeBody is basically a container, with Upper and Lower Torso. these two are
     // in fact two public pointers to the iCubUpperTorso and iCubLowerTorso objects, each
-    // having all the useful methods for each limb. To access to a specific limb, you must 
+    // having all the useful methods for each limb. To access to a specific limb, you must
     // 'address' it as a string. The string name is quite familiar, it recalls yarp ports..
     cout<<endl
         <<"iCub has many DOF: "<<endl
@@ -92,8 +92,8 @@ int main()
     Vector q_torso(icub.lowerTorso->getNLinks("torso"));
     Vector q_lleg(icub.lowerTorso->getNLinks("left_leg"));
     Vector q_rleg(icub.lowerTorso->getNLinks("right_leg"));
-    
-    //We can initialize the values to random to test that 
+
+    //We can initialize the values to random to test that
     //iDyn and iDynTree return the same results
     yarp::os::Random rng;
     rng.seed((int)yarp::os::Time::now());
@@ -103,7 +103,7 @@ int main()
     set_random_vector(q_torso,rng);
     set_random_vector(q_lleg,rng);
     set_random_vector(q_rleg,rng);
-    
+
     // here we set the inertial sensor (head) measurements
     Vector w0(3); Vector dw0(3); Vector ddp0(3);
     w0=dw0=ddp0=0.0; ddp0[2]=9.81;
@@ -112,7 +112,7 @@ int main()
     Matrix FM_up(6,3); FM_up.zero();
     Matrix FM_lo(6,2); FM_lo.zero();
 
-    // here we set the joints position, velocity and acceleration 
+    // here we set the joints position, velocity and acceleration
     // for all the limbs!
     q_head = icub.upperTorso->setAng("head",q_head);
     q_rarm = icub.upperTorso->setAng("right_arm",q_rarm);
@@ -137,12 +137,12 @@ int main()
     icub.lowerTorso->setD2Ang("torso",q_torso);
     icub.lowerTorso->setD2Ang("right_leg",q_rleg);
     icub.lowerTorso->setD2Ang("left_leg",q_lleg);
-    
-    // initialize the head with the kinematic measures retrieved 
+
+    // initialize the head with the kinematic measures retrieved
     // by the inertial sensor on the head
     icub.upperTorso->setInertialMeasure(w0,dw0,ddp0);
-    
-        
+
+
     // solve the kinematic of all the limbs attached to the upper torso
     // i.e. head, right arm and left arm
     // then solve the wrench phase, where the external wrenches are set
@@ -151,12 +151,12 @@ int main()
     // note: FM_up = 6x3, because we have 3 limbs with external wrench input
     // note: afterAttach=false is default
     Matrix fm_sens_up = icub.upperTorso->estimateSensorsWrench(FM_up,false);
-    
+
     // connect upper and lower torso: they exchange kinematic and wrench information
     icub.lowerTorso->setInertialMeasure(icub.upperTorso->getTorsoAngVel(),icub.upperTorso->getTorsoAngAcc(),icub.upperTorso->getTorsoLinAcc());
 
- 
-    
+
+
     // now solve lower torso the same way of the upper
     // note: FM_lo = 6x2, because we have 2 limbs with external wrench input to be set
     // since the torso receives the external wrench output from the upperTorso node
@@ -189,14 +189,16 @@ int main()
     ////////////////////////////////////////////////////////////////////
     //// iDyn
     ////////////////////////////////////////////////////////////////////
-    
-    
-    
+
+
+
     // declare an iCubTree
     iCubTree_version_tag version;
     version.head_version = ver.head_version;
     version.legs_version = ver.legs_version;
-    
+    //Feet ft are not present in iDyn
+    version.feet_ft = false;
+
     //Use the same serialization as used in IDYN_SERIALIZATION
     iCubTree icub_tree(version,IDYN_SERIALIZATION);
 
@@ -211,7 +213,7 @@ int main()
         <<" - right leg : "<<icub_tree.getNrOfDOFs("right_leg")<<endl<<endl;
 
 
-    // here we set the joints position, velocity and acceleration 
+    // here we set the joints position, velocity and acceleration
     // for all the limbs!
     icub_tree.setAng(q_head,"head");
     icub_tree.setAng(q_rarm,"right_arm");
@@ -236,20 +238,20 @@ int main()
     icub_tree.setD2Ang(q_torso,"torso");
     icub_tree.setD2Ang(q_rleg,"right_leg");
     icub_tree.setD2Ang(q_lleg,"left_leg");
-    
-    // initialize the head with the kinematic measures retrieved 
+
+    // initialize the head with the kinematic measures retrieved
     // by the inertial sensor on the head
     icub_tree.setInertialMeasure(w0,dw0,ddp0);
-    
+
     icub_tree.kinematicRNEA();
     icub_tree.dynamicRNEA();
-    
+
     Vector left_arm_ft(6), right_arm_ft(6), left_leg_ft(6), right_leg_ft(6);
     icub_tree.getSensorMeasurement(0,left_arm_ft);
     icub_tree.getSensorMeasurement(1,right_arm_ft);
     icub_tree.getSensorMeasurement(2,left_leg_ft);
     icub_tree.getSensorMeasurement(3,right_leg_ft);
-    
+
     Vector com(3);
     double mass;
     icub.computeCOM();
@@ -264,7 +266,7 @@ int main()
     <<" left  : "<<left_leg_ft.toString()<<endl
     <<" right : "<<right_leg_ft.toString()<<endl
     <<endl
-    
+
     <<"Torques in upper body" << endl
     <<" head : " << icub_tree.getTorques("head").toString() << endl
     <<" left arm: " << icub_tree.getTorques("left_arm").toString() << endl
@@ -274,8 +276,8 @@ int main()
     <<" left leg: " << icub_tree.getTorques("left_leg").toString() << endl
     <<" right leg: " << icub_tree.getTorques("right_leg").toString() << endl;
 
-    
-    
+
+
     cout <<"Difference in estimate FT sensor measurements on upper body (iDyn return the force of the parent on the child, while iDynTree and the real robot the one of the child on the parent): "<<endl
     <<" left  : "<<(fm_sens_up.getCol(1) +left_arm_ft).toString()<<endl
     <<" right : "<<(fm_sens_up.getCol(0) +right_arm_ft).toString()<<endl
@@ -284,7 +286,7 @@ int main()
     <<" left  : "<<(fm_sens_lo.getCol(1)+left_leg_ft).toString()<<endl
     <<" right : "<<(fm_sens_lo.getCol(0)+right_leg_ft).toString()<<endl
     <<endl
-    
+
 
     <<"Difference in torques in upper body" << endl
     <<" head : " << (icub.upperTorso->getTorques("head").subVector(0,2)-icub_tree.getTorques("head")).toString() << endl
@@ -307,29 +309,29 @@ int main()
     << "Difference in COM " << (com-icub_tree.getCOM()).toString() << std::endl
     << "Original COM " << com.toString() << std::endl
     << "iDynTree COM " << ((icub_tree.getCOM())).toString() << std::endl;
-    
+
     if( norm(fm_sens_up.getCol(1) +left_arm_ft) > tol ) { return EXIT_FAILURE; }
     if( norm(fm_sens_up.getCol(0) +right_arm_ft) > tol ) { return EXIT_FAILURE; }
     if( norm((fm_sens_lo.getCol(1)+left_leg_ft)) > tol ) { return EXIT_FAILURE; }
     if( norm(fm_sens_lo.getCol(0)+right_leg_ft) > tol ) { return EXIT_FAILURE; }
-    
+
     if( norm(icub.upperTorso->getTorques("head").subVector(0,2)-icub_tree.getTorques("head")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.upperTorso->getTorques("left_arm")-icub_tree.getTorques("left_arm")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.upperTorso->getTorques("right_arm")-icub_tree.getTorques("right_arm")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.lowerTorso->getTorques("left_leg")-icub_tree.getTorques("left_leg")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.lowerTorso->getTorques("right_leg")-icub_tree.getTorques("right_leg")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.upperTorso->getAng("head").subVector(0,2)-icub_tree.getAng("head")) > tol ) { return EXIT_FAILURE; }
-    
+
     if( norm(icub.upperTorso->getAng("left_arm")-icub_tree.getAng("left_arm")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.upperTorso->getAng("right_arm")-icub_tree.getAng("right_arm")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.lowerTorso->getAng("torso")-icub_tree.getAng("torso")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.lowerTorso->getAng("left_leg")-icub_tree.getAng("left_leg")) > tol ) { return EXIT_FAILURE; }
     if( norm(icub.lowerTorso->getAng("right_leg")-icub_tree.getAng("right_leg")) > tol ) { return EXIT_FAILURE; }
-    
+
     if( norm(com-icub_tree.getCOM()) > tol ) { return EXIT_FAILURE; }
-    
+
     return EXIT_SUCCESS;
 }
-      
-      
+
+
 
