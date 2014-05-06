b1af168c8e980634951c5454bc190fbba73874cd
Silvio Traversaro <silvio.traversaro@iit.it>
2014-05-06 15:44:17 +0200

    Fixed bug in toKDL* regressor_utils functions, added relative tests and bumped minor version

--- tests/regressor_utils_test.cpp
+++ tests/regressor_utils_test.cpp
@@ -1,5 +1,6 @@
 #include <kdl_codyco/regressor_utils.hpp>
 
+#include <kdl/frames_io.hpp>
 #include <iostream>
 
 using namespace KDL;
@@ -13,23 +14,72 @@ double random_double()
 typedef Eigen::Matrix<double,6,1> Vector6d;
 
 int main()
-{   
-    
+{
+    double tol = 1e-10;
+
    //Testing WrenchTransformationMatrix
    Wrench random_wrench(Vector(random_double(),random_double(),random_double()),Vector(random_double(),random_double(),random_double()));
    KDL::Frame random_frame(Frame::DH(random_double(),random_double(),random_double(),random_double()));
    Wrench random_result = random_frame*random_wrench;
    Vector6d random_result_eigen1,random_result_eigen2;
-   
+
    random_result_eigen1 = toEigen(random_result);
    random_result_eigen2 = WrenchTransformationMatrix(random_frame)*toEigen(random_wrench);
-   
+
    //std::cout << random_result_eigen1 << std::endl;
    //std::cout << random_result_eigen2 << std::endl;
    //std::cout << random_result_eigen1-random_result_eigen2 << std::endl;
-   
+
    if( (random_result_eigen1-random_result_eigen2).norm() > 1e-12 ) { return -1; }
-   
-    
+
+   //Testing toKDLWrench
+   Vector6d random_wrench_converted = toEigen(random_wrench);
+   Wrench random_wrench_double_conversion = toKDLWrench(random_wrench_converted);
+
+   std::cout << "Random wrench: " << std::endl;
+   std::cout << random_wrench << std::endl;
+   std::cout << random_wrench_converted << std::endl;
+   std::cout << random_wrench_double_conversion << std::endl;
+
+
+   for(int i=0; i < 6; i++ )
+   {
+       if( fabs(random_wrench_converted(i) - random_wrench(i)) > tol )
+       {
+           return -1;
+       }
+
+       if( fabs(random_wrench_double_conversion(i) - random_wrench(i)) > tol )
+       {
+           return -1;
+       }
+   }
+
+   //Testing toKDLTwist
+   Twist random_twist(Vector(random_double(),random_double(),random_double()),Vector(random_double(),random_double(),random_double()));
+   Vector6d random_twist_converted = toEigen(random_twist);
+   Twist random_twist_double_conversion = toKDLTwist(random_twist_converted);
+
+
+   std::cout << "Random twist: " << std::endl;
+   std::cout << random_twist << std::endl;
+   std::cout << random_twist_converted << std::endl;
+   std::cout << random_twist_double_conversion << std::endl;
+
+
+   for(int i=0; i < 6; i++ )
+   {
+       if( fabs(random_twist_converted(i) - random_twist(i)) > tol )
+       {
+           return -1;
+       }
+
+       if( fabs(random_twist_double_conversion(i) - random_twist(i)) > tol )
+       {
+           return -1;
+       }
+   }
+
+
    return 0;
 }
