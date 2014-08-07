a5970236cb5b23959f33a8cb543f513bc59c1cd9
Stephane Lallee <stephane.lallee@gmail.com>
2014-08-07 15:50:48 +0200

    Improved FindEigen3 file

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -13,7 +13,8 @@
 # This module reads hints about search locations from 
 # the following enviroment variables:
 #
-# EIGEN3_ROOT - Preferred Eigen3 directory 
+# EIGEN3_ROOT
+# EIGEN3_ROOT_DIR
 
 # Copyright (c) 2006, 2007 Montel Laurent, <montel@kde.org>
 # Copyright (c) 2008, 2009 Gael Guennebaud, <g.gael@free.fr>
@@ -65,10 +66,12 @@ if (EIGEN3_INCLUDE_DIR)
   set(EIGEN3_FOUND ${EIGEN3_VERSION_OK})
 
 else (EIGEN3_INCLUDE_DIR)
+  message(DEBUG "Searching eigen also on " $ENV{EIGEN3_ROOT_DIR})
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       HINTS
-      ENV EIGEN3_ROOT
+      ENV EIGEN3_ROOT 
+	  ENV EIGEN3_ROOT_DIR
       PATHS
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
