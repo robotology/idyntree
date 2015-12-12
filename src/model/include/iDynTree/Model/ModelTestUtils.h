1aa964570cdfd6b29eeeefab8f1f4b0499db2d92
Silvio Traversaro <silvio.traversaro@iit.it>
2015-12-12 17:18:39 +0100

    [model] Refactor function for generating random models out of ModelUnitTest

---
+++ src/model/include/iDynTree/Model/ModelTestUtils.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 Fondazione Istituto Italiano di Tecnologia
+ * Authors: Silvio Traversaro
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#ifndef IDYNTREE_MODEL_TEST_UTILS_H
+#define IDYNTREE_MODEL_TEST_UTILS_H
+
+
+#include <iDynTree/Model/Model.h>
+#include <iDynTree/Model/FixedJoint.h>
+#include <iDynTree/Model/RevoluteJoint.h>
+
+#include <iDynTree/Core/TestUtils.h>
+
+#include <cassert>
+
+namespace iDynTree
+{
+
+Link getRandomLink()
+{
+    double cxx = getRandomDouble(0,3);
+    double cyy = getRandomDouble(0,4);
+    double czz = getRandomDouble(0,6);
+    double rotInertiaData[3*3] = {czz+cyy,0.0,0.0,
+                                  0.0,cxx+czz,0.0,
+                                  0.0,0.0,cxx+cyy};
+
+    Rotation rot = Rotation::RPY(getRandomDouble(),getRandomDouble(-1,1),getRandomDouble());
+
+    SpatialInertia inertiaLink(getRandomDouble(0,4),
+                               Position(getRandomDouble(-2,2),getRandomDouble(-2,2),getRandomDouble(-2,2)),
+                               rot*RotationalInertiaRaw(rotInertiaData,3,3));
+
+    Link link;
+
+    link.setInertia(inertiaLink);
+
+    return link;
+}
+
+/**
+ * Add a random link with random model.
+ */
+void addRandomLinkToModel(Model & model, std::string parentLink, std::string newLinkName)
+{
+    // Add Link
+    LinkIndex newLinkIndex = model.addLink(newLinkName,getRandomLink());
+
+    // Now add joint
+    LinkIndex parentLinkIndex = model.getLinkIndex(parentLink);
+
+    int nrOfJointTypes = 2;
+
+    int jointType = rand() % nrOfJointTypes;
+
+    jointType = 1;
+
+    if( jointType == 0 )
+    {
+        FixedJoint fixJoint(parentLinkIndex,newLinkIndex,getRandomTransform());
+        model.addJoint(newLinkName+"joint",&fixJoint);
+    }
+    else if( jointType == 1 )
+    {
+        RevoluteJoint revJoint(parentLinkIndex,newLinkIndex,getRandomTransform(),getRandomAxis());
+        model.addJoint(newLinkName+"joint",&revJoint);
+    }
+    else
+    {
+        assert(false);
+    }
+}
+
+std::string getRandomLinkOfModel(const Model & model)
+{
+    int nrOfLinks = model.getNrOfLinks();
+
+    LinkIndex randomLink = rand() % nrOfLinks;
+
+    return model.getLinkName(randomLink);
+}
+
+std::string int2string(int i)
+{
+    std::stringstream ss;
+
+    ss << i;
+
+    return ss.str();
+}
+
+Model getRandomModel(unsigned int nrOfJoints)
+{
+    Model model;
+
+    model.addLink("baseLink",getRandomLink());
+
+    for(unsigned int i=0; i < nrOfJoints; i++)
+    {
+        std::string parentLink = getRandomLinkOfModel(model);
+        std::string linkName = "link" + int2string(i);
+        addRandomLinkToModel(model,parentLink,linkName);
+    }
+
+    return model;
+}
+
+Model getRandomChain(unsigned int nrOfJoints)
+{
+    Model model;
+
+    model.addLink("baseLink",getRandomLink());
+
+    std::string linkName = "baseLink";
+    for(unsigned int i=0; i < nrOfJoints; i++)
+    {
+        std::string parentLink = linkName;
+        linkName = "link" + int2string(i);
+        addRandomLinkToModel(model,parentLink,linkName);
+    }
+
+    return model;
+}
+
+}
+
+#endif /* IDYNTREE_LINK_H */
