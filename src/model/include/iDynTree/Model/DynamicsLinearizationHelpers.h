6fd3fb27b87c6579f1fa4e7b5fd9eeedae1420b7
Silvio Traversaro <silvio.traversaro@iit.it>
2015-12-12 17:11:41 +0100

    [model] helpers class for dynamics linearization functions

---
+++ src/model/include/iDynTree/Model/DynamicsLinearizationHelpers.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2015 Fondazione Istituto Italiano di Tecnologia
+ * Authors: Silvio Traversaro
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#ifndef IDYNTREE_DYNAMICS_LINEARIZATION_HELPERS_H
+#define IDYNTREE_DYNAMICS_LINEARIZATION_HELPERS_H
+
+#include <iDynTree/Core/MatrixFixSize.h>
+
+#include <iDynTree/Model/Dynamics.h>
+
+namespace iDynTree
+{
+    /**
+     * Class representing the derivative
+     * of a spatial motion vector with respect to
+     * another spatial motion vector.
+     */
+    class SpatialMotionWrtMotionDerivative: public Matrix6x6
+    {
+    public:
+        /**
+         * Equivalent to:
+         *
+         * ```
+         * SpatialMotionWrtMotionDerivative ret;
+         * toEigen(ret) = toEigen(ret)*toEigen(transform.asAdjointMatrix())
+         * return ret;
+         * ```
+         */
+        SpatialMotionWrtMotionDerivative operator*(const Transform & a_X_b);
+
+     };
+
+     /**
+       * Equivalent to:
+       *
+       * ```
+       * SpatialForceWrtMotionDerivative ret;
+       * toEigen(ret) = toEigen(transform.asAdjointMatrixWrench())*toEigen(ret);
+       * return ret;
+       * ```
+       */
+    SpatialMotionWrtMotionDerivative operator*(const Transform & a_X_b, const SpatialMotionWrtMotionDerivative & op2);
+
+
+    /**
+     * Class representing the derivative
+     * of a spatial force vector with respect to
+     * a spatial motion vector.
+     */
+    class SpatialForceWrtMotionDerivative: public Matrix6x6
+    {
+    public:
+        /**
+         * Equivalent to:
+         *
+         * ```
+         * SpatialForceWrtMotionDerivative ret;
+         * toEigen(ret) = toEigen(ret)*toEigen(transform.asAdjointMatrix())
+         * return ret;
+         * ```
+         */
+        SpatialForceWrtMotionDerivative operator*(const Transform & a_X_b);
+
+    };
+
+
+     /**
+      * Equivalent to:
+      *
+      * ```
+      * SpatialForceWrtMotionDerivative ret;
+      * toEigen(ret) = toEigen(transform.asAdjointMatrixWrench())*toEigen(ret);
+      * return ret;
+      * ```
+      */
+     SpatialForceWrtMotionDerivative operator*(const Transform & a_X_b, const SpatialForceWrtMotionDerivative & op2);
+
+}
+
+
+#endif /* IDYNTREE_DYNAMICS_LINEARIZATION_HELPERS_H */
