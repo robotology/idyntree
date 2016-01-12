0b6d3486e5ddd11d73f5d129ce589f9ba5b23dbb
Silvio <pegua1@gmail.com>
2016-01-12 18:18:58 +0000

    Added yarp component for code that depends just on YARP

---
+++ src/yarp/src/YARPConversions.cpp
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2013 IIT - Istituto Italiano di Tecnologia
+ * Author: Silvio Traversaro
+ * CopyPolicy: Released under the terms of the GNU LGPL v2.0 (or any later version)
+ *
+ */
+
+#include <iDynTree/yarp/YARPConversions.h>
+
+#include <iDynTree/Core/Direction.h>
+#include <iDynTree/Core/VectorDynSize.h>
+
+#include <yarp/math/Math.h>
+#include <cstring>
+
+using namespace yarp::math;
+
+namespace iDynTree
+{
+
+bool toiDynTree(const yarp::sig::Vector & yarpVector, iDynTree::Wrench & iDynTreeWrench)
+{
+    if( yarpVector.size() != 6 )
+    {
+        return false;
+    }
+
+    memcpy(iDynTreeWrench.getLinearVec3().data(),yarpVector.data(),3*sizeof(double));
+    memcpy(iDynTreeWrench.getAngularVec3().data(),yarpVector.data()+3,3*sizeof(double));
+    return true;
+}
+
+
+bool toYarp(const iDynTree::Wrench & iDynTreeWrench,yarp::sig::Vector & yarpVector)
+{
+    if( yarpVector.size() != 6 )
+    {
+        yarpVector.resize(6);
+    }
+
+    memcpy(yarpVector.data(),iDynTreeWrench.getLinearVec3().data(),3*sizeof(double));
+    memcpy(yarpVector.data()+3,iDynTreeWrench.getAngularVec3().data(),3*sizeof(double));
+    return true;
+}
+
+bool toiDynTree(const yarp::sig::Vector& yarpVector, iDynTree::Position& iDynTreePosition)
+{
+    if( yarpVector.size() != 3 )
+    {
+        return false;
+    }
+
+    memcpy(iDynTreePosition.data(),yarpVector.data(),3*sizeof(double));
+    return true;
+}
+
+bool toiDynTree(const yarp::sig::Vector& yarpVector, iDynTree::Vector3& iDynTreeVector3)
+{
+    if( yarpVector.size() != 3 )
+    {
+        return false;
+    }
+
+    memcpy(iDynTreeVector3.data(),yarpVector.data(),3*sizeof(double));
+    return true;
+}
+
+bool toYarp(const iDynTree::Position& iDynTreePosition, yarp::sig::Vector& yarpVector)
+{
+    if( yarpVector.size() != 3 )
+    {
+        yarpVector.resize(3);
+    }
+
+    memcpy(yarpVector.data(),iDynTreePosition.data(),3*sizeof(double));
+    return true;
+}
+
+bool toiDynTree(const yarp::sig::Vector& yarpVector, Direction& direction)
+{
+    if( yarpVector.size() != 3 )
+    {
+        return false;
+    }
+
+    memcpy(direction.data(),yarpVector.data(),3*sizeof(double));
+
+    // normalize
+    direction.Normalize();
+
+    return true;
+}
+
+bool toYarp(const Vector3& iDynTreeVector3, yarp::sig::Vector& yarpVector)
+{
+    if( yarpVector.size() != 3 )
+    {
+        yarpVector.resize(3);
+    }
+
+    memcpy(yarpVector.data(),iDynTreeVector3.data(),3*sizeof(double));
+    return true;
+}
+
+bool toiDynTree(const yarp::sig::Vector& yarpVector, VectorDynSize& iDynTreeVector)
+{
+    iDynTreeVector.resize(yarpVector.size());
+    memcpy(iDynTreeVector.data(),yarpVector.data(),yarpVector.size()*sizeof(double));
+    return true;
+
+}
+
+
+
+
+}
+
+
+
+
