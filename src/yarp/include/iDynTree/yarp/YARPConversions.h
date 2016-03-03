99609270c02740afd5dbe33f09ab722d64474e5c
Silvio Traversaro <silvio.traversaro@iit.it>
2016-03-03 21:58:09 +0100

    [yarp] Add conversion functions for fixed size vectors and matrices

--- src/yarp/include/iDynTree/yarp/YARPConversions.h
+++ src/yarp/include/iDynTree/yarp/YARPConversions.h
@@ -57,7 +57,7 @@ bool toiDynTree(const yarp::sig::Vector & yarpVector, iDynTree::Vector3 & iDynTr
  * @param yarpVector yarp::sig::Vector output
  * @return true if conversion was sucessful, false otherwise
  */
-bool toYarp(const iDynTree::Position & iDynTreeWrench, yarp::sig::Vector & yarpVector);
+bool toYarp(const iDynTree::Position & iDynTreePosition, yarp::sig::Vector & yarpVector);
 
 /**
  * Convert a yarp::sig::Vector of 3 elements to a iDynTree::Direction
@@ -80,11 +80,99 @@ bool toYarp(const iDynTree::Vector3 & iDynTreeDirection, yarp::sig::Vector & yar
 /**
  * Convert a yarp::sig::Vector to a iDynTree::VectorDynSize
  * @param yarpVector yarp::sig::Vector input
- * @param iDynTreeWrench iDynTree::VectorDynSize output
+ * @param iDynTreeVector iDynTree::VectorDynSize output
  * @return true if conversion was successful, false otherwise
  * \note the output VectorDynSize will be resized if necessary.
  */
 bool toiDynTree(const yarp::sig::Vector & yarpVector, iDynTree::VectorDynSize & iDynTreeVector);
+
+/**
+ * Convert a iDynTree::VectorFixSize to a yarp::sig::Vector
+ * @param iDynTreeVector iDynTree::VectorFixSize input
+ * @param yarpVector yarp::sig::Vector output
+ * \note the output yarp::sig::Vector will be resized if necessary.
+ */
+template <typename VectorType>
+void toYarp(const VectorType& iDynTreeVector, yarp::sig::Vector& yarpVector)
+{
+    size_t vecSize = iDynTreeVector.size();
+    if( yarpVector.size() != vecSize )
+    {
+        yarpVector.resize(vecSize);
+    }
+
+    memcpy(yarpVector.data(),iDynTreeVector.data(),vecSize*sizeof(double));
+}
+
+/**
+ * Convert a iDynTree::MatrixFixSize to a yarp::sig::Matrix
+ * @param iDynTreeMatrix iDynTree::MatrixFixSize input
+ * @param yarpMatrix yarp::sig::Matrix output
+ * \note the output yarp::sig::Matrix will be resized if necessary.
+ */
+template <typename MatrixType>
+void toYarp(const MatrixType& iDynTreeMatrix, yarp::sig::Matrix& yarpMatrix)
+{
+    size_t rows = iDynTreeMatrix.rows();
+    size_t cols = iDynTreeMatrix.cols();
+
+    if( yarpMatrix.rows() != rows ||
+        yarpMatrix.cols() != cols )
+    {
+        yarpMatrix.resize(rows,cols);
+    }
+
+    // Here we explot the fact that both YARP and iDynTree store
+    // Matrices in row major forma .
+    memcpy(yarpMatrix.data(),iDynTreeMatrix.data(),rows*cols*sizeof(double));
+}
+
+/**
+ * Convert a yarp::sig::Vector to a iDynTree::VectorFixSize
+ * @param yarpVector yarp::sig::Vector input
+ * @param iDynTreeVector iDynTree::VectorFixSize output
+ * @return true if conversion was successful, false otherwise
+ *        (if the input yarpMatrix has size different from the output VectorFixSize)
+ */
+template <typename VectorType>
+bool toiDynTree(const yarp::sig::Vector& yarpVector, VectorType& iDynTreeVector)
+{
+    size_t vecSize = iDynTreeVector.size();
+    if( yarpVector.size() != vecSize )
+    {
+        return false;
+    }
+
+    memcpy(iDynTreeVector.data(),yarpVector.data(),vecSize*sizeof(double));
+    return true;
+}
+
+/**
+ * Convert a yarp::sig::Matrix to a iDynTree::MatrixFixSize
+ * @param yarpMatrix yarp::sig::Matrix input
+ * @param iDynTreeMatrix iDynTree::MatrixFixSize output
+ * @return true if conversion was successful, false otherwise
+ *        (if the input yarpMatrix has size different from the MatrixFixSize)
+ */
+template <typename MatrixType>
+bool toiDynTree(const yarp::sig::Matrix& yarpMatrix, MatrixType& iDynTreeMatrix)
+{
+    size_t rows = iDynTreeMatrix.rows();
+    size_t cols = iDynTreeMatrix.cols();
+
+    if( yarpMatrix.rows() != rows ||
+        yarpMatrix.cols() != cols )
+    {
+        return false;
+    }
+
+    // Here we explot the fact that both YARP and iDynTree store
+    // Matrices in row major forma .
+    memcpy(iDynTreeMatrix.data(),yarpMatrix.data(),rows*cols*sizeof(double));
+
+    return true;
+}
+
 
 }
 
