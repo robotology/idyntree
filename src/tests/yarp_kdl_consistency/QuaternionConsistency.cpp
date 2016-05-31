2f9ddbfa2b4c9ab83822b644522eb0f1442c94a5
Silvio <pegua1@gmail.com>
2016-05-31 18:09:00 +0100

    Fix QuaternionConsistency test, disabling test against YARP that seems to be inconsistent

--- src/tests/yarp_kdl_consistency/QuaternionConsistency.cpp
+++ src/tests/yarp_kdl_consistency/QuaternionConsistency.cpp
@@ -19,6 +19,8 @@
 #include <yarp/sig/Matrix.h>
 #include <yarp/math/Math.h>
 
+#include <Eigen/Geometry>
+
 #include <cstdlib>
 #include <limits>
 #include <iostream>
@@ -81,12 +83,18 @@ struct QuaternionMaxMinHelper
     QuaternionMaxMin idyntree;
     QuaternionMaxMin yarp;
     QuaternionMaxMin kdl;
+    QuaternionMaxMin eigen;
 };
 
 void getQuaternion(const iDynTree::Rotation & rot,
                    iDynTree::Vector4 & quat_idyntree,
                    iDynTree::Vector4 & quat_kdl,
-                   iDynTree::Vector4 & quat_yarp)
+                   iDynTree::Vector4 & quat_yarp,
+                   iDynTree::Vector4 & quat_eigen,
+                   iDynTree::Rotation & rotBack_idyntree,
+                   iDynTree::Rotation & rotBack_kdl,
+                   iDynTree::Rotation & rotBack_yarp,
+                   iDynTree::Rotation & rotBack_eigen)
 {
     rot.getQuaternion(quat_idyntree);
 
@@ -101,41 +109,68 @@ void getQuaternion(const iDynTree::Rotation & rot,
     quat_yarp_yarp = yarp::math::dcm2quat(rotYarp);
 
     iDynTree::toiDynTree(quat_yarp_yarp,quat_yarp);
+
+    Eigen::Quaternion<double> quat_eigen_eigen = Eigen::Quaternion<double>(iDynTree::toEigen(rot));
+
+    // Eigen internally stores quaternions as imaginary/real
+    quat_eigen(0) = quat_eigen_eigen.w();
+    quat_eigen(1) = quat_eigen_eigen.x();
+    quat_eigen(2) = quat_eigen_eigen.y();
+    quat_eigen(3) = quat_eigen_eigen.z();
+
+    // We convert the quaternion back to rotations always using the iDynTree function, to make sure that all quaternions are compatible
+    rotBack_idyntree = iDynTree::Rotation::RotationFromQuaternion(quat_idyntree);
+    rotBack_kdl = iDynTree::Rotation::RotationFromQuaternion(quat_kdl);
+    rotBack_yarp = iDynTree::Rotation::RotationFromQuaternion(quat_yarp);
+    rotBack_eigen = iDynTree::Rotation::RotationFromQuaternion(quat_eigen);
+
 }
 
 void checkRotationToQuaternion(const iDynTree::Rotation randRot, QuaternionMaxMinHelper & helper, bool verbose)
 {
-    iDynTree::Vector4 quat_idyntree, quat_kdl, quat_yarp;
-    getQuaternion(randRot,quat_idyntree,quat_kdl,quat_yarp);
-
-    if( verbose )
-    {
-        std::cerr << "Quaternion converted by KDL: " << quat_kdl.toString() << " ( norm " << toEigen(quat_kdl).norm() <<  " ) " << std::endl;
-        std::cerr << "Quaternion converted by YARP: " << quat_yarp.toString() << " ( norm " << toEigen(quat_yarp).norm() <<  " ) " << std::endl;
-        std::cerr << "Quaternion converted by iDynTree: " << quat_idyntree.toString() << " ( norm " << toEigen(quat_idyntree).norm() <<  " ) " << std::endl;
-    }
-
-    // ASSERT_EQUAL_VECTOR(quat_kdl,quat_yarp);
-    // ASSERT_EQUAL_VECTOR(quat_idyntree,quat_kdl);
-    // ASSERT_EQUAL_VECTOR(quat_idyntree,quat_yarp);
-
-
-    // Check going back to a rotation
-    iDynTree::Rotation randRotCheck = iDynTree::Rotation::RotationFromQuaternion(quat_idyntree);
+    iDynTree::Vector4 quat_idyntree, quat_kdl, quat_yarp, quat_eigen;
+    iDynTree::Rotation rotBack_idyntree, rotBack_kdl, rotBack_yarp, rotBack_eigen;
+    getQuaternion(randRot,quat_idyntree,quat_kdl,quat_yarp,quat_eigen,rotBack_idyntree,rotBack_kdl,rotBack_yarp,rotBack_eigen);
 
     if( verbose )
     {
         std::cerr << "Original rotation:\n" << randRot.toString() << std::endl;
-        std::cerr << "Rotation converted back and forth from quaternion:\n" << randRotCheck.toString() << std::endl;
+        std::cerr << "Quaternion converted by iDynTree: " << quat_idyntree.toString() << " ( norm " << toEigen(quat_idyntree).norm() <<  " ) " << std::endl;
+        std::cerr << "Quaternion converted by KDL: " << quat_kdl.toString() << " ( norm " << toEigen(quat_kdl).norm() <<  " ) " << std::endl;
+        std::cerr << "Quaternion converted by YARP: " << quat_yarp.toString() << " ( norm " << toEigen(quat_yarp).norm() <<  " ) " << std::endl;
+        std::cerr << "Quaternion converted by Eigen: " << quat_eigen.toString() << " ( norm " << toEigen(quat_eigen).norm() <<  " ) " << std::endl;
     }
-    // ASSERT_EQUAL_MATRIX_TOL(randRot,randRotCheck);
+
+    // Only iDynTree and KDL share the same "positive real part" assumption for quaternions 
+    ASSERT_EQUAL_VECTOR_TOL(quat_idyntree,quat_kdl,1e-7);
+    /*
+    ASSERT_EQUAL_VECTOR(quat_kdl,quat_yarp);
+    ASSERT_EQUAL_VECTOR(quat_idyntree,quat_yarp);
+    */
+
+    if( verbose )
+    {
+        std::cerr << "Rotation converted back and forth from quaternion by iDynTree :\n" << rotBack_idyntree.toString() << std::endl;
+        std::cerr << "Rotation converted back and forth from quaternion by KDL :\n" << rotBack_kdl.toString() << std::endl;
+        std::cerr << "Rotation converted back and forth from quaternion by YARP :\n" << rotBack_yarp.toString() << std::endl;
+        std::cerr << "Rotation converted back and forth from quaternion by Eigen:\n" << rotBack_eigen.toString() << std::endl;
+    }
+
+    ASSERT_EQUAL_MATRIX_TOL(randRot,rotBack_idyntree,1e-7);
+    ASSERT_EQUAL_MATRIX_TOL(randRot,rotBack_kdl,1e-7);
+    // Commented out until we sort out the problem in YARP matrix
+    //ASSERT_EQUAL_MATRIX_TOL(randRot,rotBack_yarp,1e-7);
+    ASSERT_EQUAL_MATRIX_TOL(randRot,rotBack_eigen,1e-7);
+
 
     // Check range
     helper.idyntree.update(quat_idyntree);
     helper.kdl.update(quat_kdl);
     helper.yarp.update(quat_yarp);
+    helper.eigen.update(quat_eigen);
 
 }
+
 
 int main()
 {
@@ -149,21 +184,16 @@ int main()
 
     // Then we check two rotation, to see if any implementation suffer of the
     // discontinuities reported in http://it.mathworks.com/matlabcentral/answers/164746-bug-in-dcm2quat-function
-    iDynTree::Rotation T_1059 = iDynTree::Rotation(0.4218,-0.3220,0.8475,
-                                                   0.7299,-0.4339,-0.5282,
-                                                   0.5378,0.8414,0.0520);
+    iDynTree::Rotation T_positiveTrace = iDynTree::Rotation(0,-1,0,
+                                                            1,0,0,
+                                                            0,0,1);
 
-    iDynTree::Rotation T_1060 = iDynTree::Rotation(0.4312,-0.2603,0.8639,
-                                                   0.7289,-0.4637,-0.5036,
-                                                   0.5317,0.8469,-0.0102);
-                                                   
-   iDynTree::Rotation T_fritz_1 = iDynTree::Rotation(0,-1,0,
-                                                  1,0,0,
-                                                  0,0,1);
+    iDynTree::Rotation T_negativeTrace = iDynTree::Rotation(-1,0,0,
+                                                             0,0,1,
+                                                             0,1,0);
 
-    checkRotationToQuaternion(T_1059,rangeHelper,true);
-    checkRotationToQuaternion(T_1060,rangeHelper,true);
-    checkRotationToQuaternion(T_fritz_1,rangeHelper,true);
+    checkRotationToQuaternion(T_positiveTrace,rangeHelper,true);
+    checkRotationToQuaternion(T_negativeTrace,rangeHelper,true);
 
     // Then some random ones
     int nrOfTrials = 100000;
@@ -177,12 +207,20 @@ int main()
         checkRotationToQuaternion(randRot,rangeHelper,false);
     }
 
+    std::cerr << "iDynTree quaternion ranges: " << std::endl;
+    std::cerr << rangeHelper.idyntree.toString();
+
     std::cerr << "KDL quaternion ranges: " << std::endl;
     std::cerr << rangeHelper.kdl.toString();
 
     std::cerr << "YARP quaternion ranges: " << std::endl;
     std::cerr << rangeHelper.yarp.toString();
 
+    std::cerr << "Eigen quaternion ranges: " << std::endl;
+    std::cerr << rangeHelper.eigen.toString();
+
 
     return EXIT_SUCCESS;
 }
+
+
