3a750960cbb8a0e1f23b1080038d2ed46324edb5
Silvio <pegua1@gmail.com>
2016-07-21 13:26:23 +0200

    [core] Added helpers function to efficently set subvectors and submatrices in EigenHelpers

--- src/core/include/iDynTree/Core/EigenHelpers.h
+++ src/core/include/iDynTree/Core/EigenHelpers.h
@@ -15,6 +15,7 @@
 #include <iDynTree/Core/MatrixFixSize.h>
 #include <iDynTree/Core/SpatialMotionVector.h>
 #include <iDynTree/Core/SpatialForceVector.h>
+#include <iDynTree/Core/Utils.h>
 
 namespace iDynTree
 {
@@ -108,6 +109,100 @@ inline Eigen::Matrix<typename Derived::Scalar, 3, 3, Eigen::RowMajor> skew(const
                                                                              -vec[1], vec[0], 0.0).finished();
 }
 
+
+/**
+ * Submatrix helpers
+ */
+template<unsigned int nRows, unsigned int nCols>
+inline void setSubMatrix(MatrixDynSize& mat,
+                         const IndexRange rowRange,
+                         const IndexRange colRange,
+                         const MatrixFixSize<nRows,nCols>& subMat)
+{
+
+    toEigen(mat).block(rowRange.offset,colRange.offset,rowRange.size,colRange.size) = toEigen(subMat);
+    return;
 }
 
-#endif /* IDYNTREE_VECTOR_DYN_SIZE_H */
+template<typename T>
+inline void setSubMatrix(MatrixDynSize& mat,
+                         const IndexRange rowRange,
+                         const IndexRange colRange,
+                         const T& subMat)
+{
+    std::cerr << "Setting block " << rowRange.offset << " " << colRange.offset << " " << rowRange.size << " " << colRange.size << std::endl;
+    std::cerr << "to " << subMat.rows() << " " << subMat.cols() << std::endl;
+
+    std::cerr <<  toEigen(mat).block(rowRange.offset,colRange.offset,rowRange.size,colRange.size) << std::endl;
+    toEigen(mat).block(rowRange.offset,colRange.offset,rowRange.size,colRange.size) = subMat;
+    return;
+}
+
+inline void setSubMatrix(MatrixDynSize& mat,
+                         const IndexRange rowRange,
+                         const IndexRange colRange,
+                         const double subMat)
+{
+    assert(rowRange.size == 1);
+    assert(colRange.size == 1);
+    mat(rowRange.offset,colRange.offset) = subMat;
+    return;
+}
+
+inline void setSubMatrixToIdentity(MatrixDynSize& mat,
+                                   const IndexRange rowRange,
+                                   const IndexRange colRange)
+{
+    assert(rowRange.size == colRange.size);
+    for(int i=0; i < rowRange.size; i++)
+    {
+        mat(rowRange.offset+i,colRange.offset+i) = 1.0;
+    }
+    return;
+}
+
+inline void setSubMatrixToMinusIdentity(MatrixDynSize& mat,
+                                        const IndexRange rowRange,
+                                        const IndexRange colRange)
+{
+    assert(rowRange.size == colRange.size);
+    for(int i=0; i < rowRange.size; i++)
+    {
+        mat(rowRange.offset+i,colRange.offset+i) = -1.0;
+    }
+    return;
+}
+
+
+template<unsigned int size>
+inline void setSubVector(VectorDynSize& vec,
+                         const IndexRange range,
+                         const VectorFixSize<size>& subVec)
+{
+    toEigen(vec).segment(range.offset,range.size) = toEigen(subVec);
+    return;
+}
+
+inline void setSubVector(VectorDynSize& vec,
+                         const IndexRange range,
+                         double subVec)
+{
+    assert(range.size==1);
+    vec(range.offset) = subVec;
+    return;
+}
+
+template<typename T>
+inline void setSubVector(VectorDynSize& vec,
+                         const IndexRange range,
+                         const T& subVec)
+{
+    std::cerr << "Setting block " << range.offset << " " << range.size << std::endl;
+    std::cerr << "To " << subVec << std::endl;
+    toEigen(vec).segment(range.offset,range.size) = subVec;
+    return;
+}
+
+}
+
+#endif /* IDYNTREE_EIGEN_HELPERS_H */
