11b16e94a10bfc39eb5d1e9efe0ccdaee635dab8
Silvio <pegua1@gmail.com>
2016-07-21 13:28:01 +0200

    [sensors] Added header containing the headers of all sensor classes

---
+++ src/sensors/include/iDynTree/Sensors/AllSensorsTypes.h
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2016 Fondazione Istituto Italiano di Tecnologia
+ * Authors: Silvio Traversaro
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+
+ */
+
+#ifndef IDYNTREE_ALL_SENSORS_TYPE_H
+#define IDYNTREE_ALL_SENSORS_TYPE_H
+
+#include <iDynTree/Sensors/SixAxisFTSensor.h>
+#include <iDynTree/Sensors/AccelerometerSensor.h>
+#include <iDynTree/Sensors/GyroscopeSensor.h>
+
+#endif
