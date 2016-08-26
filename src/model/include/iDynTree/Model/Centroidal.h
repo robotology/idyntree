7ae2e2eaac867576aa7b030a89cf76bc5cbd8cda
Silvio <pegua1@gmail.com>
2016-08-26 20:12:20 +0200

    Implemented AverageVelocity and Total Momentum Jacobian

---
+++ src/model/include/iDynTree/Model/Centroidal.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 Fondazione Istituto Italiano di Tecnologia
+ * Authors: Silvio Traversaro
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ *
+ */
+
+#ifndef IDYNTREE_CENTROIDAL_H
+#define IDYNTREE_CENTROIDAL_H
+
+#include <iDynTree/Model/Indeces.h>
+
+namespace iDynTree
+{
+    class Model;
+    class Traversal;
+    class Transform;
+    class FreeFloatingPos;
+    class FreeFloatingVel;
+    class FreeFloatingAcc;
+    class LinkPositions;
+    class LinkVelArray;
+    class LinkAccArray;
+    class JointPosDoubleArray;
+    class MatrixDynSize;
+
+
+}
+
+#endif
