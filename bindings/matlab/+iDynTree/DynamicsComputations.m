d0b64ed6f9fe1fded2931a5ac8b96ce8752832bd
Silvio <pegua1@gmail.com>
2017-03-10 09:38:00 +0100

    Regenerate matlab bindings

--- bindings/matlab/+iDynTree/DynamicsComputations.m
+++ bindings/matlab/+iDynTree/DynamicsComputations.m
@@ -9,118 +9,118 @@ classdef DynamicsComputations < SwigRef
           self.swigPtr = varargin{1}.swigPtr;
         end
       else
-        tmp = iDynTreeMEX(1603, varargin{:});
+        tmp = iDynTreeMEX(1614, varargin{:});
         self.swigPtr = tmp.swigPtr;
         tmp.swigPtr = [];
       end
     end
     function delete(self)
       if self.swigPtr
-        iDynTreeMEX(1604, self);
+        iDynTreeMEX(1615, self);
         self.swigPtr=[];
       end
     end
     function varargout = loadRobotModelFromFile(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1605, self, varargin{:});
-    end
-    function varargout = loadRobotModelFromString(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1606, self, varargin{:});
-    end
-    function varargout = isValid(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1607, self, varargin{:});
-    end
-    function varargout = getNrOfDegreesOfFreedom(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1608, self, varargin{:});
-    end
-    function varargout = getDescriptionOfDegreeOfFreedom(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1609, self, varargin{:});
-    end
-    function varargout = getDescriptionOfDegreesOfFreedom(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1610, self, varargin{:});
-    end
-    function varargout = getNrOfLinks(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1611, self, varargin{:});
-    end
-    function varargout = getNrOfFrames(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1612, self, varargin{:});
-    end
-    function varargout = getFloatingBase(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1613, self, varargin{:});
-    end
-    function varargout = setFloatingBase(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1614, self, varargin{:});
-    end
-    function varargout = setRobotState(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1615, self, varargin{:});
-    end
-    function varargout = getWorldBaseTransform(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1616, self, varargin{:});
     end
-    function varargout = getBaseTwist(self,varargin)
+    function varargout = loadRobotModelFromString(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1617, self, varargin{:});
     end
-    function varargout = getJointPos(self,varargin)
+    function varargout = isValid(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1618, self, varargin{:});
     end
-    function varargout = getJointVel(self,varargin)
+    function varargout = getNrOfDegreesOfFreedom(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1619, self, varargin{:});
     end
-    function varargout = getFrameIndex(self,varargin)
+    function varargout = getDescriptionOfDegreeOfFreedom(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1620, self, varargin{:});
     end
-    function varargout = getFrameName(self,varargin)
+    function varargout = getDescriptionOfDegreesOfFreedom(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1621, self, varargin{:});
     end
-    function varargout = getWorldTransform(self,varargin)
+    function varargout = getNrOfLinks(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1622, self, varargin{:});
     end
-    function varargout = getRelativeTransform(self,varargin)
+    function varargout = getNrOfFrames(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1623, self, varargin{:});
     end
-    function varargout = getFrameTwist(self,varargin)
+    function varargout = getFloatingBase(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1624, self, varargin{:});
     end
-    function varargout = getFrameTwistInWorldOrient(self,varargin)
+    function varargout = setFloatingBase(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1625, self, varargin{:});
     end
-    function varargout = getFrameProperSpatialAcceleration(self,varargin)
+    function varargout = setRobotState(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1626, self, varargin{:});
     end
-    function varargout = getLinkIndex(self,varargin)
+    function varargout = getWorldBaseTransform(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1627, self, varargin{:});
     end
-    function varargout = getLinkInertia(self,varargin)
+    function varargout = getBaseTwist(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1628, self, varargin{:});
     end
-    function varargout = getJointIndex(self,varargin)
+    function varargout = getJointPos(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1629, self, varargin{:});
     end
-    function varargout = getJointName(self,varargin)
+    function varargout = getJointVel(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1630, self, varargin{:});
     end
-    function varargout = getJointLimits(self,varargin)
+    function varargout = getFrameIndex(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1631, self, varargin{:});
     end
-    function varargout = inverseDynamics(self,varargin)
+    function varargout = getFrameName(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1632, self, varargin{:});
     end
-    function varargout = getFreeFloatingMassMatrix(self,varargin)
+    function varargout = getWorldTransform(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1633, self, varargin{:});
     end
-    function varargout = getFrameJacobian(self,varargin)
+    function varargout = getRelativeTransform(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1634, self, varargin{:});
     end
-    function varargout = getDynamicsRegressor(self,varargin)
+    function varargout = getFrameTwist(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1635, self, varargin{:});
     end
-    function varargout = getModelDynamicsParameters(self,varargin)
+    function varargout = getFrameTwistInWorldOrient(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1636, self, varargin{:});
     end
-    function varargout = getCenterOfMass(self,varargin)
+    function varargout = getFrameProperSpatialAcceleration(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1637, self, varargin{:});
     end
-    function varargout = getCenterOfMassJacobian(self,varargin)
+    function varargout = getLinkIndex(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1638, self, varargin{:});
+    end
+    function varargout = getLinkInertia(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1639, self, varargin{:});
+    end
+    function varargout = getJointIndex(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1640, self, varargin{:});
+    end
+    function varargout = getJointName(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1641, self, varargin{:});
+    end
+    function varargout = getJointLimits(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1642, self, varargin{:});
+    end
+    function varargout = inverseDynamics(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1643, self, varargin{:});
+    end
+    function varargout = getFreeFloatingMassMatrix(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1644, self, varargin{:});
+    end
+    function varargout = getFrameJacobian(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1645, self, varargin{:});
+    end
+    function varargout = getDynamicsRegressor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1646, self, varargin{:});
+    end
+    function varargout = getModelDynamicsParameters(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1647, self, varargin{:});
+    end
+    function varargout = getCenterOfMass(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1648, self, varargin{:});
+    end
+    function varargout = getCenterOfMassJacobian(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1649, self, varargin{:});
     end
   end
   methods(Static)
--- bindings/matlab/+iDynTree/IModelVisualization.m
+++ bindings/matlab/+iDynTree/IModelVisualization.m
@@ -5,42 +5,45 @@ classdef IModelVisualization < SwigRef
     end
     function delete(self)
       if self.swigPtr
-        iDynTreeMEX(1569, self);
+        iDynTreeMEX(1577, self);
         self.swigPtr=[];
       end
     end
     function varargout = setPositions(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1570, self, varargin{:});
-    end
-    function varargout = setLinkPositions(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1571, self, varargin{:});
-    end
-    function varargout = model(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1572, self, varargin{:});
-    end
-    function varargout = getInstanceName(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1573, self, varargin{:});
-    end
-    function varargout = setModelVisibility(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1574, self, varargin{:});
-    end
-    function varargout = setModelColor(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1575, self, varargin{:});
-    end
-    function varargout = resetModelColor(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1576, self, varargin{:});
-    end
-    function varargout = getLinkNames(self,varargin)
-      [varargout{1:nargout}] = iDynTreeMEX(1577, self, varargin{:});
-    end
-    function varargout = setLinkVisibility(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1578, self, varargin{:});
     end
-    function varargout = getFeatures(self,varargin)
+    function varargout = setLinkPositions(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1579, self, varargin{:});
     end
-    function varargout = setFeatureVisibility(self,varargin)
+    function varargout = model(self,varargin)
       [varargout{1:nargout}] = iDynTreeMEX(1580, self, varargin{:});
+    end
+    function varargout = getInstanceName(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1581, self, varargin{:});
+    end
+    function varargout = setModelVisibility(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1582, self, varargin{:});
+    end
+    function varargout = setModelColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1583, self, varargin{:});
+    end
+    function varargout = resetModelColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1584, self, varargin{:});
+    end
+    function varargout = getLinkNames(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1585, self, varargin{:});
+    end
+    function varargout = setLinkVisibility(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1586, self, varargin{:});
+    end
+    function varargout = getFeatures(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1587, self, varargin{:});
+    end
+    function varargout = setFeatureVisibility(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1588, self, varargin{:});
+    end
+    function varargout = jets(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1589, self, varargin{:});
     end
     function self = IModelVisualization(varargin)
       if nargin==1 && strcmp(class(varargin{1}),'SwigRef')
