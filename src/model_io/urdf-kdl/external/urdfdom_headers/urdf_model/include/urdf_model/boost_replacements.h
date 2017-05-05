611ade7b6d94a5a9a4d1ab4d0678d8a3d834477d
Silvio Traversaro <silvio.traversaro@iit.it>
2017-05-05 16:05:08 +0200

    [model_io] Fix implicit dependency of URDF parsers on system locale
    See https://github.com/robotology/idyntree/issues/288

--- src/model_io/urdf-kdl/external/urdfdom_headers/urdf_model/include/urdf_model/boost_replacements.h
+++ src/model_io/urdf-kdl/external/urdfdom_headers/urdf_model/include/urdf_model/boost_replacements.h
@@ -48,24 +48,29 @@ namespace urdf {
  * Convert a string to a double
  *
  */
-bool inline stringToDouble(const std::string & inStr, double & outDouble)
+bool inline stringToDoubleWithClassicLocale(const std::string & inStr, double & outDouble)
 {
-    outDouble = std::atof(inStr.c_str());
-    return true;
+    std::istringstream ss(inStr);
+    ss.imbue(std::locale::classic());
+    ss >> outDouble;
+    return !(ss.fail());
 }
 
-bool inline stringToInt(const std::string & inStr, int & outInt)
+bool inline stringToIntWithClassicLocale(const std::string & inStr, int & outInt)
 {
-    outInt = std::atoi(inStr.c_str());
-    return true;
+    std::istringstream ss(inStr);
+    ss.imbue(std::locale::classic());
+    ss >> outInt;
+    return !(ss.fail());
 }
 
-bool inline stringToUnsignedInt(const std::string & inStr, unsigned int & outInt)
+bool inline stringToUnsignedIntWithClassicLocale(const std::string & inStr, unsigned int & outInt)
 {
-    outInt = (unsigned int)std::atoi(inStr.c_str());
-    return true;
+    std::istringstream ss(inStr);
+    ss.imbue(std::locale::classic());
+    ss >> outInt;
+    return !(ss.fail());
 }
-
 std::string inline intToString(const int inInt)
 {
     std::stringstream ss;
