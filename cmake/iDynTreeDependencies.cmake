e0d7949bf87eb87aeb8ad26ae2a7eb5db990a9c4
Silvio Traversaro <pegua1@gmail.com>
2017-08-27 13:50:45 +0100

    Fix handling of Eigen version in CMake
    The issues fixed are:
      * We need to remaing compatible with Eigen 3.2.92 as it is the version shipped in Xenial (3.3-beta2).
      * The order of the find_package version argument was wrong.
      * The FindEigen3.cmake was not givien error if the version requirement was changed after EIGEN3_INCLUDE_DIR was already defined.

--- cmake/iDynTreeDependencies.cmake
+++ cmake/iDynTreeDependencies.cmake
@@ -24,8 +24,8 @@ macro(idyntree_handle_dependency package)
   endif ()
 endmacro ()
 
-# Eigen is compulsory (minimum version 3.3)
-find_package(Eigen3 REQUIRED 3.3)
+# Eigen is compulsory (minimum version 3.2.92)
+find_package(Eigen3 3.2.92 REQUIRED)
 
 # For orocos_kdl we have custom logic, because we want to set it to FALSE by default
 option(IDYNTREE_USES_KDL "Build the part of iDynTree that depends on package orocos_kdl" FALSE)
