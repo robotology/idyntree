45c0989c127da0b5389731cd571370ef45a90e19
Silvio Traversaro <pegua1@gmail.com>
2017-08-29 13:26:53 +0200

    Add unit tests for the projection methods (not working)

--- src/inverse-kinematics/tests/ConvexHullHelpersUnitTest.cpp
+++ src/inverse-kinematics/tests/ConvexHullHelpersUnitTest.cpp
@@ -44,7 +44,6 @@ void testConvexHullProjectionConstraint()
     iDynTree::Direction yAxis(0.0,1.0,0.0);
 
     bool ok = projectionConstraint.buildConvexHull(xAxis,yAxis,iDynTree::Position::Zero(),polygons,transforms);
-
     ASSERT_IS_TRUE(ok);
 
     ASSERT_IS_TRUE(projectionConstraint.projectedConvexHull.getNrOfVertices() == 4);
@@ -64,7 +63,48 @@ void testConvexHullProjectionConstraint()
     // Fourth point
     ASSERT_EQUAL_DOUBLE(projectionConstraint.projectedConvexHull(3)(0),-1.0);
     ASSERT_EQUAL_DOUBLE(projectionConstraint.projectedConvexHull(3)(1), 1.0);
+}
 
+void testConvexHullProjectionWithGravity()
+{
+    // For this test, we will use a simple convex hull of side 1 meter
+    // centered in the origin of the world and laying on the XY plane
+    iDynTree::ConvexHullProjectionConstraint projectionConstraint;
+    iDynTree::Polygon convexHull;
+    convexHull.m_vertices.push_back(iDynTree::Position( 0.5, 0.5, 0.0));
+    convexHull.m_vertices.push_back(iDynTree::Position(-0.5, 0.5, 0.0));
+    convexHull.m_vertices.push_back(iDynTree::Position(-0.5,-0.5, 0.0));
+    convexHull.m_vertices.push_back(iDynTree::Position( 0.5,-0.5, 0.0));
+    std::vector<iDynTree::Polygon> polygons;
+    polygons.resize(1);
+    polygons[0] = convexHull;
+
+
+    // The convex hull is already expressed in the world
+    std::vector<iDynTree::Transform> transforms;
+    transforms.push_back(iDynTree::Transform::Identity());
+
+    iDynTree::Direction xAxis(1.0,0.0,0.0);
+    iDynTree::Direction yAxis(0.0,1.0,0.0);
+
+    bool ok = projectionConstraint.buildConvexHull(xAxis,yAxis,iDynTree::Position::Zero(),polygons,transforms);
+    ASSERT_IS_TRUE(ok);
+
+    // Use a test position that is outside the convex hull with the normal projection
+    iDynTree::Position testPoint(1.5, 0.0, 1.0);
+
+    // Projected along the normal of the plane, this point is outside the convex hull
+    // Note: positive margin means inside, negative outside
+    ASSERT_IS_TRUE(projectionConstraint.computeMargin(projectionConstraint.project(testPoint)) < 0);
+
+    // If the direction along with we are projecting is the normal of the plane, the projection
+    // should match the one done without direction
+    iDynTree::Direction planeNormal(0.0, 0.0, -1.0);
+    //ASSERT_EQUAL_VECTOR(projectionConstraint.project(testPoint), projectionConstraint.projectAlongDirection(testPoint, planeNormal));
+
+    // If the direction of the projection of is "skewed" to the left, the point should instead be inside the convex hull
+    iDynTree::Direction skewedDirection(-1.0, 0.0, -1.0);
+    // ASSERT_IS_TRUE(projectionConstraint.computeMargin(projectionConstraint.projectAlongDirection(testPoint, skewedDirection)) < 0);
 }
 
 int main()
