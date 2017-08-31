81d919a85e011140c724b39e9c9db52c8c8c4bb6
Diego Ferigo <diego.ferigo@iit.it>
2017-08-31 09:55:08 +0000

    Added TransformFromMatrix4x4 unit test

---
+++ src/core/tests/TransformFromMatrix4x4UnitTest.cpp
@@ -0,0 +1,33 @@
+#include <iDynTree/Core/TestUtils.h>
+#include <iDynTree/Core/Transform.h>
+#include <iDynTree/Core/Position.h>
+#include <iDynTree/Core/Rotation.h>
+#include <iDynTree/Core/MatrixFixSize.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace iDynTree;
+
+void checkFromMatrix4x4()
+{
+    RotationRaw rot_raw(-2.449, -1.224, -2.041, 0.408, -0.707, -0.707,  0.816, 0.409, -1.154);
+    PositionRaw pos_raw(1, 2, 3);
+    double transform[16] = {-2.449, -1.224, -2.041, 1, 0.408, -0.707, -0.707, 2, 0.816, 0.409, -1.154, 3, 0, 0, 0, 1};
+
+    Rotation rot = Rotation(rot_raw);
+    Position pos = Position(pos_raw);
+
+    Matrix4x4 homogeneousMatrix(transform, 4, 4);
+
+    Transform t_posrot(rot, pos);
+    Transform t_buffer(homogeneousMatrix);
+
+    ASSERT_EQUAL_TRANSFORM(t_posrot, t_buffer);
+}
+
+int main()
+{
+    checkFromMatrix4x4();
+    return EXIT_SUCCESS;
+}
