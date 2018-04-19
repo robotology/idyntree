fd9efa9536bf836259f09b65ae8693a43dddd1f7
Prashanth Ramadoss <prashanthramadoss@gmail.com>
2018-04-19 17:11:22 +0200

    [GravComp] added copyright header

--- src/estimation/include/iDynTree/Estimation/GravityCompensationHelpers.h
+++ src/estimation/include/iDynTree/Estimation/GravityCompensationHelpers.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia
+ *
+ * Licensed under either the GNU Lesser General Public License v3.0 :
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ * or the GNU Lesser General Public License v2.1 :
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * at your option.
+ */
+
 #ifndef GRAVITY_COMPENSATION_HELPERS_H
 #define GRAVITY_COMPENSATION_HELPERS_H
 
@@ -120,3 +130,4 @@ namespace iDynTree
   };
 }
 #endif
+
