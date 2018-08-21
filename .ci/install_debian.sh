6421ea83f813e2f4011df8413761be7218687a31
Silvio Traversaro <silvio.traversaro@iit.it>
2018-08-21 10:58:44 +0200

    Migrate Travis to use docker to test on Xenial and Bionic

---
+++ .ci/install_debian.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+apt-get update
+
+# noninteractive tzdata ( https://stackoverflow.com/questions/44331836/apt-get-install-tzdata-noninteractive )
+export DEBIAN_FRONTEND=noninteractive
+
+# CI specific packages
+apt-get install -y clang valgrind
+
+# Dependencies
+apt-get install -y build-essential cmake coinor-libipopt-dev liborocos-kdl-dev libeigen3-dev libtinyxml-dev libace-dev libgsl0-dev libopencv-dev libode-dev liblua5.1-dev lua5.1 git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev
+
+# Lua
+apt-get install liblua5.2-dev
+
+# Python
+apt-get install -y python-dev
+
+# Octave
+apt-get install -y liboctave-dev
+
+
+
+
+
