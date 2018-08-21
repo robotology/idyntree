e8334df7fe22e618e6fe54ae5bd381023e12ff27
Silvio Traversaro <silvio.traversaro@iit.it>
2018-08-21 22:10:14 +0200

    Update install_debian.sh

--- .ci/install_debian.sh
+++ .ci/install_debian.sh
@@ -13,7 +13,7 @@ apt-get install -y clang valgrind
 apt-get install -y build-essential cmake coinor-libipopt-dev liborocos-kdl-dev libeigen3-dev libtinyxml-dev libace-dev libgsl0-dev libopencv-dev libode-dev git swig qtbase5-dev qtdeclarative5-dev qtmultimedia5-dev libqt5opengl5-dev libqcustomplot-dev libxml2-dev liburdfdom-dev
 
 # Lua
-apt-get install -y liblua5.2-dev lua5.2
+apt-get install -y liblua5.1-dev lua5.1
 
 # Python
 apt-get install -y python-dev
