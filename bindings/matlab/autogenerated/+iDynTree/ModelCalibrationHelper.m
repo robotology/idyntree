a517d9c5ca634ad9f832cf865f19b18e500dbcc5
Silvio Traversaro <silvio.traversaro@iit.it>
2018-10-01 10:37:12 +0200

    Add autogenerated bindings

---
+++ bindings/matlab/autogenerated/+iDynTree/ModelCalibrationHelper.m
@@ -0,0 +1,47 @@
+classdef ModelCalibrationHelper < SwigRef
+  methods
+    function this = swig_this(self)
+      this = iDynTreeMEX(3, self);
+    end
+    function self = ModelCalibrationHelper(varargin)
+      if nargin==1 && strcmp(class(varargin{1}),'SwigRef')
+        if ~isnull(varargin{1})
+          self.swigPtr = varargin{1}.swigPtr;
+        end
+      else
+        tmp = iDynTreeMEX(1444, varargin{:});
+        self.swigPtr = tmp.swigPtr;
+        tmp.SwigClear();
+      end
+    end
+    function delete(self)
+      if self.swigPtr
+        iDynTreeMEX(1445, self);
+        self.SwigClear();
+      end
+    end
+    function varargout = loadModelFromString(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1446, self, varargin{:});
+    end
+    function varargout = loadModelFromFile(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1447, self, varargin{:});
+    end
+    function varargout = updateModelInertialParametersToString(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1448, self, varargin{:});
+    end
+    function varargout = updateModelInertialParametersToFile(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1449, self, varargin{:});
+    end
+    function varargout = model(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1450, self, varargin{:});
+    end
+    function varargout = sensors(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1451, self, varargin{:});
+    end
+    function varargout = isValid(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1452, self, varargin{:});
+    end
+  end
+  methods(Static)
+  end
+end
---
+++ bindings/matlab/autogenerated/+iDynTree/estimateInertialParametersFromLinkBoundingBoxesAndTotalMass.m
@@ -0,0 +1,3 @@
+function varargout = estimateInertialParametersFromLinkBoundingBoxesAndTotalMass(varargin)
+  [varargout{1:nargout}] = iDynTreeMEX(1229, varargin{:});
+end
