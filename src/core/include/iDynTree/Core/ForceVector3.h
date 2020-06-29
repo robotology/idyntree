2e4eda07418ffffb7252b71f44f6bf93a247906c
Silvio Traversaro <silvio.traversaro@iit.it>
2020-06-29 11:29:06 +0200

    Add again deprecated headers
    This headers were removed, but they are still used in some downstream projects
    and were not clearly deprecated in iDynTree 1 . For this reason we add them back,
    with clear deprecation notices.

---
+++ src/core/include/iDynTree/Core/ForceVector3.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) Fondazione Istituto Italiano di Tecnologia
+ *
+ * Licensed under either the GNU Lesser General Public License v3.0 :
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ * or the GNU Lesser General Public License v2.1 :
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * at your option.
+ */
+
+#ifndef IDYNTREE_FORCE_VECTOR_3_H
+#define IDYNTREE_FORCE_VECTOR_3_H
+
+#warning <iDynTree/Core/ForceVector3.h> is deprecated. Please use GeomVector3 and <iDynTree/Model/GeomVector3.h>.
+
+#include <iDynTree/Core/GeomVector3.h>
+
+#endif /* IDYNTREE_FORCE_VECTOR_3_H */
