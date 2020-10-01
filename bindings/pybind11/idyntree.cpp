cf10dae0e307b775d44852f642e595a3defab450
Francesco Romano <francesco.romano.1987@gmail.com>
2020-10-01 17:06:13 +0100

    Create pybind11 Python bindings for idyntree-modelio-urdf.

--- bindings/pybind11/idyntree.cpp
+++ bindings/pybind11/idyntree.cpp
@@ -2,6 +2,7 @@
 
 #include "idyntree_core.h"
 #include "idyntree_model.h"
+#include "idyntree_modelio_urdf.h"
 #include "idyntree_sensors.h"
 
 #include <pybind11/pybind11.h>
@@ -15,6 +16,7 @@ PYBIND11_MODULE(bindings, m) {
   iDynTree::bindings::iDynTreeCoreBindings(m);
   iDynTree::bindings::iDynTreeModelBindings(m);
   iDynTree::bindings::iDynTreeSensorsBindings(m);
+  iDynTree::bindings::iDynTreeModelIoUrdfBindings(m);
 }
 
 }  // namespace
