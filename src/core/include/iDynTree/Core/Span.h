6b56a088bd4c6748920b4ba2d298af864c6fdf37
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-10-12 22:25:11 +0200

    Bugfix in Span class

--- src/core/include/iDynTree/Core/Span.h
+++ src/core/include/iDynTree/Core/Span.h
@@ -495,8 +495,8 @@ public:
         return data()[idx];
     }
 
-    IDYNTREE_CONSTEXPR double getVal(index_type idx) const { return this->operator[](idx);}
-    IDYNTREE_CONSTEXPR bool setVal(index_type idx, double val)
+    IDYNTREE_CONSTEXPR const reference getVal(index_type idx) const { return this->operator[](idx);}
+    IDYNTREE_CONSTEXPR bool setVal(index_type idx, const reference val)
     {
         assert(idx >= 0 && idx < storage_.size());
         data()[idx] = val;
