d337204fa745b9b28cac986727a5f3d7bc5c9fd8
Stefano <stefano.dafarra@gmail.com>
2021-01-22 19:47:37 +0100

    Removed duplicated documentation.

--- src/visualization/src/Texture.h
+++ src/visualization/src/Texture.h
@@ -26,41 +26,14 @@ public:
     irr::video::ITexture* irrTexture{nullptr};
     Environment textureEnvironment;
 
-    /**
-     * @brief Initialize texture
-     * @param Irrlicht driver
-     * @param name Name of the texture
-     * @param textureOptions Textures options
-     */
     void init(irr::video::IVideoDriver* irrDriver, irr::scene::ISceneManager *sceneManager, const std::string& name, const VisualizerOptions& textureOptions);
 
-    /**
-     * Destructor
-     */
     virtual ~Texture();
 
-    /**
-     * Return an interface to manipulate the texture environment.
-     */
     virtual IEnvironment& environment();
 
-    /**
-     * @brief Get the color of the pixel at the given position in the additional texture.
-     *
-     * Remember to call draw() first.
-     * @param width The width of the pixel
-     * @param height The height of the pixel
-     * @return The color of the pixel
-     */
     virtual ColorViz getPixelColor(unsigned int width, unsigned int height) const;
 
-    /**
-     * @brief Get the pixels of the texture.
-     *
-     * Remember to call draw() first.
-     * @param pixels The output pixels
-     * @return True in case of success, false otherwise
-     */
     virtual bool getPixels(std::vector<PixelViz>& pixels) const;
 
 };
