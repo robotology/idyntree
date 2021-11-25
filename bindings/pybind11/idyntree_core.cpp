f04e844802f585d05db557db335dc107b5c26b38
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-11-25 21:16:09 +0100

    Implement baseVectorDefinition and baseMatrixDefinition in bindings/pybind11/idyntree_core.cpp

--- bindings/pybind11/idyntree_core.cpp
+++ bindings/pybind11/idyntree_core.cpp
@@ -25,6 +25,66 @@ namespace bindings {
 namespace {
 namespace py = ::pybind11;
 
+template <typename VectorType>
+void baseVectorDefinition(py::class_<VectorType>& vector) {
+  vector
+      .def("__getitem__", py::overload_cast<const std::size_t>(
+                              &VectorType::operator(), py::const_))
+      .def("__setitem__",
+           [](VectorType& the_vector, std::size_t index,
+              double new_value) { the_vector(index) = new_value; })
+      .def("__len__", &VectorType::size)
+      .def("__iter__",
+          [](const VectorType& s) {
+            return py::make_iterator(s.begin(), s.end());
+          },
+          py::keep_alive<
+              0, 1>() /* Essential: keep object alive while iterator exists */)
+      .def("set_zero", &VectorType::zero)
+      .def("__repr__", &VectorType::toString)
+      .def_buffer([](VectorType& v) -> py::buffer_info {
+        return py::buffer_info(
+            v.data(),                                /* Pointer to buffer */
+            sizeof(double),                          /* Size of one scalar */
+            py::format_descriptor<double>::format(), /* Python struct-style
+                                                        format descriptor */
+            1,                                       /* Number of dimensions */
+            {v.size()},                              /* Buffer dimensions */
+            {sizeof(double)} /* Strides (in bytes) for each index */
+        );
+      });
+}
+
+template <typename MatrixType>
+void baseMatrixDefinition(py::class_<MatrixType>& matrix) {
+  matrix
+      .def("__getitem__",
+           [](MatrixType& matrix,
+              std::pair<std::size_t, std::size_t> indices) {
+             return matrix.getVal(indices.first, indices.second);
+           })
+      .def("__setitem__",
+           [](MatrixType& matrix,
+              std::pair<std::size_t, std::size_t> indices, double item) {
+             return matrix.setVal(indices.first, indices.second, item);
+           })
+      .def("rows", &MatrixType::rows)
+      .def("cols", &MatrixType::cols)
+      .def("set_zero", &MatrixType::zero)
+      .def("__repr__", &MatrixType::toString)
+      .def_buffer([](MatrixType& m) -> py::buffer_info {
+        return py::buffer_info(
+            m.data(),                                /* Pointer to buffer */
+            sizeof(double),                          /* Size of one scalar */
+            py::format_descriptor<double>::format(), /* Python struct-style
+                                                        format descriptor */
+            2,                                       /* Number of dimensions */
+            {m.rows(), m.cols()},                    /* Buffer dimensions */
+            {sizeof(double) * m.cols(), /* Strides (in bytes) for each index */
+             sizeof(double)});
+      });
+}
+
 void transformClassDefinition(py::class_<Transform>& transform) {
   transform.def(py::init())
       .def(py::init<const Rotation&, const Position&>())
@@ -48,12 +108,12 @@ void transformClassDefinition(py::class_<Transform>& transform) {
 }
 }  // namespace
 
+
 void iDynTreeCoreBindings(pybind11::module& module) {
 
   // Positions, Rotations and Transforms.
-  py::class_<PositionRaw>(module, "_PositionRaw")
-      // Do not expose constructor as we do not want users to use this class.
-      .def("__repr__", &PositionRaw::toString);
+  py::class_<PositionRaw> positionRaw(module, "_PositionRaw",  py::buffer_protocol());
+  baseVectorDefinition(positionRaw);
 
   py::class_<Position, PositionRaw>(module, "Position")
       .def(py::init())
@@ -67,9 +127,8 @@ void iDynTreeCoreBindings(pybind11::module& module) {
       .def_static("Zero", &Position::Zero);
   py::implicitly_convertible<iDynTree::VectorFixSize<3>, iDynTree::Position>();
 
-  py::class_<RotationRaw>(module, "_RotationRaw")
-      // Do not expose constructor as we do not want users to use this class.
-      .def("__repr__", &RotationRaw::toString);
+  py::class_<RotationRaw> rotationRaw(module, "_RotationRaw", py::buffer_protocol());
+  baseMatrixDefinition(rotationRaw);
 
   py::class_<Rotation, RotationRaw>(module, "Rotation")
       .def(py::init())
@@ -94,8 +153,9 @@ void iDynTreeCoreBindings(pybind11::module& module) {
   transformClassDefinition(transform);
 
   // Other classes.
-  py::class_<Direction>(module, "Direction")
-      .def(py::init<double, double, double>());
+  py::class_<Direction> direction(module, "Direction", py::buffer_protocol());
+  direction.def(py::init<double, double, double>());
+  baseVectorDefinition(direction);
 
   py::class_<Axis>(module, "Axis")
       .def(py::init<const Direction&, const Position&>())
@@ -103,8 +163,10 @@ void iDynTreeCoreBindings(pybind11::module& module) {
       .def_property("origin", &Axis::getOrigin, &Axis::setOrigin)
       .def("__repr__", &Axis::toString);
 
-  py::class_<RotationalInertiaRaw>(module, "RotationalInertia")
-      .def(py::init());
+  py::class_<RotationalInertiaRaw> rotationalInertia(module, "RotationalInertia",
+                                                     py::buffer_protocol());
+  rotationalInertia.def(py::init());
+  baseMatrixDefinition(rotationalInertia);
 
   py::class_<SpatialInertiaRaw>(module, "_SpatialInertiaRaw")
       .def("from_rotational_inertia_wrt_center_of_mass",
