a431cf9d8994fc3d427e230b636768d45864db56
Giulio Romualdi <giulio.romualdi@gmail.com>
2022-10-25 12:57:00 +0200

    Update the idyntree-model-view-meshcat accordingly to b37c71c692d54b6ce90898bf2a86131fb618881f

--- bindings/python/scripts/idyntree-model-view-meshcat.py
+++ bindings/python/scripts/idyntree-model-view-meshcat.py
@@ -21,12 +21,13 @@ def main():
         description="Display the model in the meshcat visualizer."
     )
     parser.add_argument("--model", "-m", type=str, required=True, help="Model path.")
+    parser.add_argument("--alpha", "-a", type=float, required=False, help="Alpha channel of the model.")
 
     args = parser.parse_args()
 
     # Load the visualizer
     viz = MeshcatVisualizer()
-    viz.load_model_from_file(args.model)
+    viz.load_model_from_file(args.model, color=args.alpha)
     viz.open()
 
     run = True
