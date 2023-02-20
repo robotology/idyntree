9ce864370fcd58deec1c8525fa597f7b52a12a80
Silvio Traversaro <silvio.traversaro@iit.it>
2023-02-20 21:22:07 +0100

    Add simple tests for ModelLoader in Python

---
+++ bindings/python/tests/modelloader.py
@@ -0,0 +1,17 @@
+import sys
+import unittest
+import numpy as np
+import idyntree.swig as iDynTree
+import random
+
+class ModelLoaderTest(unittest.TestCase):
+    """tests"""
+    def testLoaderReducedModel(self):
+        model_loader = iDynTree.ModelLoader()
+        considered_joints_idyn = ["torso_yaw", "torso_roll"]
+        model_loader.loadReducedModelFromFile("./model.urdf", considered_joints_idyn)
+        self.assertEqual(len(considered_joints_idyn), model_loader.model().getNrOfJoints())
+
+
+if __name__ == '__main__':
+    unittest.main()
