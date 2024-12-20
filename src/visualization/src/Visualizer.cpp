3a385b0a275d748ffa043148e997909fb661f2ed
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-12-20 17:55:44 +0100

    Add support for Wayland in `iDynTree::Visualizer`

--- src/visualization/src/Visualizer.cpp
+++ src/visualization/src/Visualizer.cpp
@@ -26,6 +26,7 @@
  #define GLFW_EXPOSE_NATIVE_NSGL
 #elif defined(__linux__)
  #define GLFW_EXPOSE_NATIVE_X11
+ #define GLFW_EXPOSE_NATIVE_WAYLAND
  #define GLFW_EXPOSE_NATIVE_GLX
 #endif
 
@@ -162,7 +163,7 @@ struct Visualizer::VisualizerPimpl
 #elif defined(__APPLE__)
     id m_windowId;
 #elif defined(__linux__)
-    Window m_windowId;
+    void* m_windowId; // Pointer to either wl_surface* or X11 Window
 #endif
 #endif
 
@@ -505,8 +506,22 @@ bool Visualizer::init(const VisualizerOptions &visualizerOptions)
     pimpl->m_windowId = glfwGetCocoaWindow(pimpl->m_window);
     irrDevParams.WindowId = (void*)(pimpl->m_windowId);
 #elif defined(__linux__)
-    pimpl->m_windowId = glfwGetX11Window(pimpl->m_window);
-    irrDevParams.WindowId = (void*)(pimpl->m_windowId);
+    void* nativeWindow = nullptr;
+
+    // Try Wayland first
+    struct wl_surface* waylandWindow = glfwGetWaylandWindow(pimpl->m_window);
+    if (waylandWindow)
+    {
+        nativeWindow = static_cast<void*>(waylandWindow);
+    }
+    else
+    {
+        // Fallback to X11
+        Window x11Window = glfwGetX11Window(pimpl->m_window);
+        if (x11Window)
+            nativeWindow = static_cast<void*>(reinterpret_cast<void*>(x11Window));
+    }
+    irrDevParams.WindowId = nativeWindow;
 #endif
 
     irrDevParams.DeviceType = irr::EIDT_SDL;
